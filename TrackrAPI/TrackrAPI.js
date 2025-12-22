7d20c5ca8edad84f5dafd95d0799e10cd213e1d6
Floryan Bornet <93878409+BornetFloryan@users.noreply.github.com>
2025-12-22 12:27:35 +0100

    Fix MongoDB connection by mongodb://localhost:27017/trackrai

--- TrackrAPI/TrackrAPI.js
+++ TrackrAPI/TrackrAPI.js
@@ -47,7 +47,7 @@ const corsOptions = {
 
 // setup mongo connection
 const mongoose = require('mongoose');
-let dev_db_url = 'mongodb://localhost/trackrapi';
+let dev_db_url = 'mongodb://localhost:27017/trackrai';
 let mongoDB = process.env.MONGODB_URI || dev_db_url;
 //mongoose.set('debug',true);
 mongoose.connect(mongoDB);
